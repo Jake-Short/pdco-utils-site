3326cf77e56c348be30254f468ebd941e706f662
Jake Short <46853220+Jake-Short@users.noreply.github.com>
2022-07-27 11:38:47 -0700

    Made fields read only

--- utils/downloadByTheNumbersPdf.ts
+++ utils/downloadByTheNumbersPdf.ts
@@ -14,14 +14,23 @@ export default async function downloadByTheNumbersPdf() {
 	const observationsSubmittedToMpc = await (await fetch('/api/getNumberObservationsMpc')).text();
   
 	form.getTextField('program_years').setText(byTheNumbersPdfData.program_years);
+	form.getTextField('program_years').enableReadOnly();
 	form.getTextField('neas_discovered').setText(byTheNumbersPdfData.neas_discovered);
+	form.getTextField('neas_discovered').enableReadOnly();
 	form.getTextField('neas_greater_than_1km').setText(byTheNumbersPdfData.neas_greater_than_1km);
+	form.getTextField('neas_greater_than_1km').enableReadOnly();
 	form.getTextField('neas_greater_than_140m').setText(byTheNumbersPdfData.neas_greater_than_140m);
+	form.getTextField('neas_greater_than_140m').enableReadOnly();
 	form.getTextField('neas_within_moon').setText(byTheNumbersPdfData.neas_within_moon);
+	form.getTextField('neas_within_moon').enableReadOnly();
 	form.getTextField('observations_submitted_to_mpc').setText(observationsSubmittedToMpc);
+	form.getTextField('observations_submitted_to_mpc').enableReadOnly();
 	form.getTextField('date_generated').setText(byTheNumbersPdfData.date_generated);
+	form.getTextField('date_generated').enableReadOnly();
 	form.getTextField('current_phas').setText(byTheNumbersPdfData.current_phas);
+	form.getTextField('current_phas').enableReadOnly();
 	form.getTextField('necs_known').setText(byTheNumbersPdfData.necs_known);
+	form.getTextField('necs_known').enableReadOnly();
 
 	const encodedUri = await doc.saveAsBase64({ dataUri: true });
 
