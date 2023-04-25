2e601a0326401456cb04f15403168e3b9f7fb126
Jake Short <46853220+Jake-Short@users.noreply.github.com>
2023-04-25 11:02:00 -0700

    Fixed form filling

--- utils/downloadByTheNumbersPdf.ts
+++ utils/downloadByTheNumbersPdf.ts
@@ -13,23 +13,23 @@ export default async function downloadByTheNumbersPdf() {
 	const byTheNumbersPdfData = await (await fetch('/api/getByTheNumbersPdfData')).json();
 	const observationsSubmittedToMpc = await (await fetch('/api/getNumberObservationsMpc')).text();
   
-	form.getTextField('program_years').setText(byTheNumbersPdfData.program_years);
+	form.getTextField('program_years').setText(String(byTheNumbersPdfData.program_years));
 	form.getTextField('program_years').enableReadOnly();
-	form.getTextField('neas_discovered').setText(byTheNumbersPdfData.neas_discovered);
+	form.getTextField('neas_discovered').setText(String(byTheNumbersPdfData.neas_discovered));
 	form.getTextField('neas_discovered').enableReadOnly();
-	form.getTextField('neas_greater_than_1km').setText(byTheNumbersPdfData.neas_greater_than_1km);
+	form.getTextField('neas_greater_than_1km').setText(String(byTheNumbersPdfData.neas_greater_than_1km));
 	form.getTextField('neas_greater_than_1km').enableReadOnly();
-	form.getTextField('neas_greater_than_140m').setText(byTheNumbersPdfData.neas_greater_than_140m);
+	form.getTextField('neas_greater_than_140m').setText(String(byTheNumbersPdfData.neas_greater_than_140m));
 	form.getTextField('neas_greater_than_140m').enableReadOnly();
-	form.getTextField('neas_within_moon').setText(byTheNumbersPdfData.neas_within_moon);
+	form.getTextField('neas_within_moon').setText(String(byTheNumbersPdfData.neas_within_moon));
 	form.getTextField('neas_within_moon').enableReadOnly();
-	form.getTextField('observations_submitted_to_mpc').setText(observationsSubmittedToMpc);
+	form.getTextField('observations_submitted_to_mpc').setText(String(observationsSubmittedToMpc));
 	form.getTextField('observations_submitted_to_mpc').enableReadOnly();
-	form.getTextField('date_generated').setText(byTheNumbersPdfData.date_generated);
+	form.getTextField('date_generated').setText(String(byTheNumbersPdfData.date_generated));
 	form.getTextField('date_generated').enableReadOnly();
-	form.getTextField('current_phas').setText(byTheNumbersPdfData.current_phas);
+	form.getTextField('current_phas').setText(String(byTheNumbersPdfData.current_phas));
 	form.getTextField('current_phas').enableReadOnly();
-	form.getTextField('necs_known').setText(byTheNumbersPdfData.necs_known);
+	form.getTextField('necs_known').setText(String(byTheNumbersPdfData.necs_known));
 	form.getTextField('necs_known').enableReadOnly();
 
 	const encodedUri = await doc.saveAsBase64({ dataUri: true });
